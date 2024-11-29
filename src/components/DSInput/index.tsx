9980350b573ff47621c0b2f2f5597d962ff3ef96
Diogo Coelho <diogooliveiracoelho@gmail.com>
2024-11-28 23:13:04 -0300

    Componente de input

---
+++ src/components/DSInput/index.tsx
@@ -0,0 +1,42 @@
+import { InputProps } from "@/types/input";
+import '../../assets/sass/app.scss';
+
+const Input = (props: InputProps) => {
+  const className = (mainClass: string): string => {
+    return [
+      mainClass,
+      props.variant ?? '',
+      (props.inputSize || props['input-size']) ?? ``,
+      props.error ? `error` : ``,
+      props.align ? `align-${props.align}` : ``,
+    ].toString().replaceAll(",", " ").trim();
+  }
+
+  return (
+    <>
+      <div className={className('input-container')} >
+        <input
+          className={className('input')}
+          type={ props.type || `text` } 
+          placeholder={ props.placeholder }
+          disabled={ props.disabled || false }
+          value={props.currentValue || props['current-value']}
+          onChange={ (event) => props.handleOnChange?.({ args: event.target.value, event }) }
+          onClick={ (event) => props.handleOnClick?.(event)}
+        />
+        { props.suffix && 
+          <div className={`suffix${props.active === 'true' ? ` active` : ''}`}
+            onClick={ (event) => props.handleOnClick?.(event)}
+          >
+            <figure>
+              <img src={props.suffix} alt="Icon"/>
+            </figure>
+          </div>
+        }
+      </div>
+      { props.error && (<span>{ props.error }</span>)}
+    </>
+  )
+}
+
+export default Input;
---
+++ src/types/input.d.ts
@@ -0,0 +1,67 @@
+import { InputHTMLAttributes, MutableRefObject } from "react";
+import { IconType } from './ds_icon';
+
+type InputAttributeType =
+  "button" |
+  "checkbox" |
+  "color" |
+  "date" |
+  "datetime-local" |
+  "email" |
+  "file" |
+  "hidden" |
+  "image" |
+  "month" |
+  "number" |
+  "password" |
+  "radio" |
+  "range" |
+  "reset" |
+  "search" |
+  "submit" |
+  "tel" |
+  "text" |
+  "time" |
+  "url" |
+  "week";
+
+type InputSize =
+  'small' |
+  'normal' |
+  'medium' |
+  'large' |
+  'full';
+
+type AlignText = 
+  'center' |
+  'left' |
+  'right';
+
+type Variant = 'secondary';
+
+export enum IconType {
+  ARROW_DROP_DOWN = '/arrow_drop_down.png'
+}
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  type?: InputAttributeType,
+  align?: AlignText,
+  disabled?: boolean,
+  placeholder?: string,
+  variant?: Variant,
+  inputSize?: InputSize,
+  "input-size"?: InputSize,
+  suffix?: IconType,
+  active?: string,
+  currentValue?: string,
+  "current-value"?: string,
+  error?: string,
+  handleOnChange?: (
+    data: { 
+      args?: T | T[], 
+      event: ChangeEvent<HTMLInputElement>
+  }) => T,
+  handleOnClick?: (
+    event: MouseEvent<HTMLButtonElement, MouseEvent> 
+  ) => T
+}
